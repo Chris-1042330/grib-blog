e05ac6c47b24d1518abc086a93400d366c120b1a
Chris-1042330 <christian.aartman@gmail.com>
2023-01-22 23:43:34 +0100

    update posts.index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 {{--        <a href="{{ route('posts.create') }}" class="btn-link btn-lg mb-2">+ New Post</a>--}}
             @foreach ($posts as $post)
-                @if($post->updated_at < $post->expiration)
+{{--                @if($post->updated_at < $post->expiration)--}}
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-2xl">
                         <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
@@ -14,7 +14,7 @@
                             {{ $post->updated_at }} - {{ $post->updated_at->diffForHumans() }}
                         </span>
                 </div>
-                @endif
+{{--                @endif--}}
             @endforeach
             {{ $posts->links() }}
 
